6cf376b95dbe7a64f9b53b3ed4406d4f94984654
pixelbumper <PixelBumper@users.noreply.github.com>
2016-12-10 18:22:46 +0100

    Added MainController

--- Assets/Scripts/UI/CharacterSelection.cs
+++ Assets/Scripts/UI/CharacterSelection.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterSelection : MonoBehaviour
 {
@@ -9,20 +11,26 @@ public class CharacterSelection : MonoBehaviour
 
     private GameObject canvas;
 
+    private PlayerState player1;
+    private PlayerState player2;
+    private PlayerState player3;
+    private PlayerState player4;
 
-	void Start ()
+    void Start ()
 	{
-	    canvas = GameObject.Find("Canvas");
-	    foreach (var character in characters)
-	    {
-	        GameObject avatarGO = new GameObject(character.name);
-	        UnityEngine.UI.Button button = avatarGO.AddComponent<UnityEngine.UI.Button>();
-//	        button.
-	    }
+	    player1= new PlayerState();
+	    player2= new PlayerState();
+	    player3= new PlayerState();
+	    player4= new PlayerState();
+
+//	    GameObject.Find("Player1Button").GetComponent<Button>().onClick.AddListener(()=>ShowTutorial());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
 }
+
--- Assets/Scripts/UI/StartMenu.cs
+++ Assets/Scripts/UI/StartMenu.cs
@@ -1,19 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    EventSystem.current.SetSelectedGameObject(GameObject.Find("1 vs 1 button"));
+	    GameObject.Find("tutorial").GetComponent<Button>().onClick.AddListener(()=>ShowTutorial());
+	    GameObject.Find("1 vs 1 button").GetComponent<Button>().onClick.AddListener(()=>LoadCharacterSelection());
+	    GameObject.Find("2 vs 2 button").GetComponent<Button>().onClick.AddListener(()=>ShowTutorial());
+	    GameObject.Find("exit").GetComponent<Button>().onClick.AddListener(()=>Exit());
 	}
-	
-	// Update is called once per frame
+
+    private void Exit()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
 	void Update () {
 	    if (UnityEngine.Input.GetButtonUp("Fire1"))
 	    {
-	        Application.LoadLevel("CharacterSelection");
+
 	    }
 	}
+
+    void LoadCharacterSelection()
+    {
+        MainController.SwitchScene("CharacterSelection");
+    }
+
+    void ShowTutorial()
+    {
+        throw new NotImplementedException();
+    }
 }
