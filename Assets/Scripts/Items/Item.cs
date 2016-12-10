68f17614e1b71ccc64aff034c37ae7fe14a3cce7
pixelbumper <PixelBumper@users.noreply.github.com>
2016-12-10 22:51:25 +0100

    working on the items

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+
+    public float flingVelocity=10.2f;
+
+	// Use this for initialization
+	void Start () {
+		Fling();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Fling()
+    {
+        if (Random.value > 0.5)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(flingVelocity,flingVelocity);
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-flingVelocity,flingVelocity);
+        }
+    }
+}
---
+++ Assets/Scripts/Rats/Rat.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Rat : MonoBehaviour
+{
+
+    private CapsuleCollider collider;
+
+    public double dropchanceBattery=0.5;
+
+    private Items item;
+
+    public enum Items
+    {
+        Ammo,
+        Battery
+    }
+
+	// Use this for initialization
+	void Start ()
+	{
+	    if (Random.value > dropchanceBattery)
+	    {
+	        item = Items.Ammo;
+	    }
+	    else
+	    {
+	        item = Items.Battery;
+	    }
+	    collider = GetComponent<CapsuleCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTakeDamage()
+    {
+        GameObject go;
+        if (item == Items.Ammo)
+        {
+            go=AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/ammo");
+        }
+        else
+        {
+            go=AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/battery");
+        }
+
+        go.transform.position = transform.position;
+        go.SendMessage("Fling");
+
+        Destroy(this);
+
+    }
+
+
+}
