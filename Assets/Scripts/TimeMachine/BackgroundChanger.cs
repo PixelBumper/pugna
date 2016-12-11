8783a4f7e587eeb1f98d4cd1b04575f344bd3743
Sergio R. Lumley <lumley256@gmail.com>
2016-12-11 17:22:51 +0100

    Added really dumb background changing for time travelling

---
+++ Assets/Scripts/TimeMachine/BackgroundChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundChanger : MonoBehaviour
+{
+
+    public GameObject[] backgrounds;
+
+    public int BackgroundCount()
+    {
+        return backgrounds.Length;
+    }
+
+    public void SetIndex(int newIndex)
+    {
+        foreach (var background in backgrounds)
+        {
+            background.SetActive(false);
+        }
+        backgrounds[newIndex].SetActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/TimeMachine/GraphicalClock.cs
+++ Assets/Scripts/TimeMachine/GraphicalClock.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 
 public class GraphicalClock : MonoBehaviour
 {
-
     public static GraphicalClock instance;
 
     public int points = 0;
-    public int maxPoints = 4;
+    public int maxPoints = 3;
     public float rotationSpeed = 1.0f;
 
     private Transform childTransform;
 
     private Quaternion _quaternion;
+    private BackgroundChanger _backgroundChanger;
 
     private void Awake()
     {
@@ -22,41 +22,69 @@ public class GraphicalClock : MonoBehaviour
     }
 
     // Use this for initialization
-	void Start ()
-	{
-	    childTransform = transform.GetChild(0);
-	    _quaternion = childTransform.rotation;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	    // Uncomment the line below to be able to update the points on live
-	    // AddPoints(0);
+    void Start()
+    {
+        _backgroundChanger = GameObject.FindGameObjectWithTag("BackgroundChanger").GetComponent<BackgroundChanger>();
+        childTransform = transform.GetChild(0);
+        _quaternion = childTransform.rotation;
+    }
 
-	    childTransform.rotation = Quaternion.Lerp(childTransform.rotation, _quaternion, Time.deltaTime * rotationSpeed);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        // Uncomment the line below to be able to update the points on live
+        // AddPoints(0);
+
+        childTransform.rotation = Quaternion.Lerp(childTransform.rotation, _quaternion, Time.deltaTime * rotationSpeed);
+    }
 
     public void AddPoints(int pointsToAdd)
     {
+        int previousBackgroundIndex = IndexForPoints(points);
+
         points += pointsToAdd;
 
         if (points > maxPoints)
         {
             // One team has won!
-
-        } else if (points < -maxPoints)
+        }
+        else if (points < -maxPoints)
         {
             // The other team has won!
-
         }
         points = Math.Min(maxPoints, Math.Max(-maxPoints, points));
+        int currentBackgroundIndex = IndexForPoints(points);
 
         float rotationRequired = (maxPoints - points) / (float) maxPoints;
         _quaternion = Quaternion.Euler(0f, 0f, rotationRequired * 90f);
 
 
-        // TODO (slumley): start changing the background here if necessary
+        // start changing the background here if necessary
+        if (previousBackgroundIndex != currentBackgroundIndex)
+        {
+            _backgroundChanger.SetIndex(currentBackgroundIndex);
+        }
     }
 
+    private int IndexForPoints(int currentPoints)
+    {
+        var backgroundCount = _backgroundChanger.BackgroundCount();
+        if (currentPoints == -maxPoints)
+        {
+            return 0;
+        }
+        else if (currentPoints == 0)
+        {
+            return backgroundCount / 2;
+        }
+        else if (currentPoints == maxPoints)
+        {
+            return backgroundCount - 1;
+        }
+        else if (currentPoints < 0)
+        {
+            return 1;
+        }
+        return 3;
+    }
 }
