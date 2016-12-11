9aa9ab3097d72e6410c7e4151077772691c4d5e0
Sergio R. Lumley <lumley256@gmail.com>
2016-12-11 18:06:11 +0100

    Implemented kick

---
+++ Assets/Scripts/Character/Kick.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kick : MonoBehaviour {
+
+    public Vector2 speedDirection = Vector2.right;
+    private const float SECONDS_LIFETIME = 0.3f;
+    private GameObject shooter;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.Translate(speedDirection.x * Time.deltaTime,
+            speedDirection.y * Time.deltaTime,
+            0);
+    }
+
+    private void OnEnable()
+    {
+        Invoke("Destroy", SECONDS_LIFETIME);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke("Destroy");
+    }
+
+    public void Destroy()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void SetShooter(GameObject whoShoot)
+    {
+        shooter = whoShoot;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!Team.IsSameTeam(other.gameObject, shooter))
+        {
+            other.gameObject.SendMessage("ReceiveDamage", 1);
+            Destroy();
+        }
+    }
+}
